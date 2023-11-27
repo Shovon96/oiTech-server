73628397b559211ff826793b899aa51cb3542c7c
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-27 17:54:29 +0600

    features id get

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +45,13 @@ async function run() {
         app.get('/features', async (req, res) => {
             const result = await featureCollection.find().toArray()
             res.send(result)
+        })
+
+        app.get('/features/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await featureCollection.findOne(query)
+            res.send(result);
         })
 
         // Trending Products api
